ad9740059a913bc8c7667f4203bc09d3de8a2b01
Salmanabdullah <salmandcc@gmail.com>
2024-03-11 17:24:38 +0100

    routes, context and hooks

---
+++ backend/controllers/createAtodo.js
@@ -0,0 +1,18 @@
+import TODO from "../models/todoModel.js";
+
+// create a todo
+const createAtodo = async (req, res) => {
+  try {
+    if (!req.body.title) {
+      return res.status(400).send({ message: "Please enter a title" });
+    }
+    const newTodo = { title: req.body.title };
+    const todo = await TODO.create(newTodo);
+
+    res.status(201).send(todo);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+};
+
+export default createAtodo;
